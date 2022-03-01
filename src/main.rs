7f023379c8b8ca5efbf827a54b99752093802ab4
Edgar Leon <edgar.leon@bankflip.io>
2022-03-01 17:09:55 +0100

    wip: getting forums links

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-use scraper::Html;
+use scraper::{Html, Selector};
 
 fn main() {
     let client = reqwest::blocking::Client::new();
-    let origin_url = "https://rolisz.ro/";
+    let origin_url = "https://www.mediavida.com/foro/";
     let res = client.get(origin_url).send().unwrap();
 
     let response_text = res.text().unwrap();
-    println!("Status for {}: {}", origin_url, response_text);
+    //println!("Status for {}: {}", origin_url, response_text);
     
 
     let parsed_html = Html::parse_document(&response_text);
 
-    println!("{:?}", parsed_html);
+    //println!("{:?}", parsed_html);
+
+    let selector = &Selector::parse(".c-main > ul").unwrap();
+
+    let forums = parsed_html.select(selector).collect::<Vec<_>>();
+
+    println!("forums retrieved {:?} ", forums);
+
 }
