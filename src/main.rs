0c236a03f2cd3f80c6a9f7f1bc0f90fc51cf3554
Leos1113 <edgarleonmartin13@gmail.com>
2022-03-01 23:14:57 +0100

    refactor: move all to a function

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,31 @@ use scraper::{Html, Selector};
 
 fn main() {
     let client = reqwest::blocking::Client::new();
+
+    get_forums(client);
+    // let origin_url = "https://www.mediavida.com/foro/";
+    // let res = client.get(origin_url).send().unwrap();
+
+    // let response_text = res.text().unwrap();
+    // //println!("Status for {}: {}", origin_url, response_text);
+
+    // let parsed_html = Html::parse_document(&response_text);
+
+    // //println!("{:?}", parsed_html);
+
+    // let selector = &Selector::parse(".c-main > ul").unwrap();
+
+    // let forums = parsed_html.select(selector).collect::<Vec<_>>();
+
+    // println!("forums retrieved {:?} ", forums);
+}
+
+fn get_forums(client: reqwest::blocking::Client) {
     let origin_url = "https://www.mediavida.com/foro/";
+
     let res = client.get(origin_url).send().unwrap();
 
     let response_text = res.text().unwrap();
-    //println!("Status for {}: {}", origin_url, response_text);
-    
 
     let parsed_html = Html::parse_document(&response_text);
 
@@ -18,5 +37,4 @@ fn main() {
     let forums = parsed_html.select(selector).collect::<Vec<_>>();
 
     println!("forums retrieved {:?} ", forums);
-
 }
