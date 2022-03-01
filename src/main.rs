5a2822deea70c9cbbbc504016b610e395593f08e
Edgar Leon <edgar.leon@bankflip.io>
2022-03-01 15:47:17 +0100

    wip: parsing html starter

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use scraper::Html;
 
 fn main() {
     let client = reqwest::blocking::Client::new();
     let origin_url = "https://rolisz.ro/";
     let res = client.get(origin_url).send().unwrap();
-    println!("Status for {}: {}", origin_url, res.text().unwrap());
+
+    let response_text = res.text().unwrap();
+    println!("Status for {}: {}", origin_url, response_text);
+    
+
+    let parsed_html = Html::parse_document(&response_text);
+
+    println!("{:?}", parsed_html);
 }
