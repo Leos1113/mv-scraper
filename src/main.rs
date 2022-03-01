6155a71b02ab187989a553cb8dec7d7577b019d7
Leos1113 <edgarleonmartin13@gmail.com>
2022-03-01 23:44:23 +0100

    refactor: better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 use scraper::{Html, Selector};
+use std::error::Error;
 
 fn main() {
     let client = reqwest::blocking::Client::new();
 
-    get_forums(client);
-    // let origin_url = "https://www.mediavida.com/foro/";
-    // let res = client.get(origin_url).send().unwrap();
-
-    // let response_text = res.text().unwrap();
-    // //println!("Status for {}: {}", origin_url, response_text);
-
-    // let parsed_html = Html::parse_document(&response_text);
-
-    // //println!("{:?}", parsed_html);
-
-    // let selector = &Selector::parse(".c-main > ul").unwrap();
-
-    // let forums = parsed_html.select(selector).collect::<Vec<_>>();
-
-    // println!("forums retrieved {:?} ", forums);
+    let forums_links = match get_forums(client) {
+        Ok(links) => println!("{:?}", links),
+        Err(e) => {
+            println!("Can't get the list of links of the forums! {}", e);
+        }
+    };
 }
 
-fn get_forums(client: reqwest::blocking::Client) {
+fn get_forums(client: reqwest::blocking::Client) -> Result<Vec<String>, Box<dyn Error>> {
     let origin_url = "https://www.mediavida.com/foro/";
 
-    let res = client.get(origin_url).send().unwrap();
+    let res = client.get(origin_url).send()?;
 
-    let response_text = res.text().unwrap();
+    let response_text = res.text()?;
 
     let parsed_html = Html::parse_document(&response_text);
 
-    //println!("{:?}", parsed_html);
-
-    let selector = &Selector::parse(".c-main > ul").unwrap();
+    let selector = &Selector::parse(".c-main > ul > li > a").unwrap();
 
     let forums = parsed_html.select(selector).collect::<Vec<_>>();
 
-    println!("forums retrieved {:?} ", forums);
+    let mut forum_links: Vec<String> = Vec::new();
+
+    for forum in &forums {
+        forum_links.push(forum.value().attr("href").unwrap().to_string());
+    }
+
+    if forums.len() > 0 {
+        return Ok(forum_links);
+    } else {
+        Err("Error! Can't read the forums!!!!".into())
+    }
 }
