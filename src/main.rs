393e7e131eb3eeb67dd97dbc3d3ccbd8490aa676
Leos1113 <edgarleonmartin13@gmail.com>
2022-03-05 20:00:33 +0100

    feature: Get all info from forum, title, link and description

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
+use reqwest::{blocking::Client, blocking::Response, StatusCode};
 use scraper::{Html, Selector};
 use std::error::Error;
 
-fn main() {
-    let client = reqwest::blocking::Client::new();
-
-    let forums_links = get_forums(client).unwrap();
-
-    println!("{:?}", forums_links);
+#[derive(Debug)]
+struct Forum {
+    title: String,
+    link: String,
+    description: String,
 }
 
-fn get_forums(client: reqwest::blocking::Client) -> Result<Vec<String>, Box<dyn Error>> {
-    let origin_url = "https://www.mediavida.com/foro/";
+fn main() {
+    let client: Client = Client::new();
 
-    let res = client.get(origin_url).send()?;
+    let forums: Vec<Forum> = get_forums_info(client).unwrap();
 
-    let parsed_html = Html::parse_document(&res.text()?);
+    println!("{:?}", forums);
+}
 
-    let selector = &Selector::parse(".c-main > ul > li > a").unwrap();
+fn get_forums_info(client: Client) -> Result<Vec<Forum>, Box<dyn Error>> {
+    let url: &str = "https://www.mediavida.com/foro/";
 
-    let forums = parsed_html.select(selector).collect::<Vec<_>>();
+    let result: Response = client.get(url).send().unwrap();
 
-    let forum_links: Vec<String> = forums
-        .iter()
-        .map(|element| element.value().attr("href").unwrap().to_string())
-        .collect::<Vec<String>>();
+    let raw_html = match result.status() {
+        StatusCode::OK => result.text().unwrap(),
+        _ => panic!("Something went wrong"),
+    };
+
+    let parsed_html = Html::parse_document(&raw_html);
+
+    let link_selector = &Selector::parse(".c-main > ul > li > a").unwrap();
+    let title_selector = &Selector::parse(".info-col > strong").unwrap();
+    let description_selector = &Selector::parse(".info-col > p").unwrap();
+
+    let mut forums: Vec<Forum> = Vec::new();
+    for element in parsed_html.select(&link_selector) {
+        let link = element.value().attr("href").unwrap().to_string();
+        let mut title_element = element.select(&title_selector);
+        let mut decription_element = element.select(&description_selector);
+
+        let mut title = String::new();
+        let mut description = String::new();
+
+        match title_element.next() {
+            Some(element_ref) => {
+                title = element_ref.inner_html().to_string();
+            }
+            _ => {}
+        }
+
+        match decription_element.next() {
+            Some(element_ref) => {
+                description = element_ref.inner_html().to_string();
+            }
+            _ => {}
+        }
+
+        forums.push(Forum {
+            link: link,
+            title: title,
+            description: description,
+        });
+    }
 
     if !forums.is_empty() {
-        Ok(forum_links)
+        Ok(forums)
     } else {
         Err("Error! Can't read the forums!!!!".into())
     }
